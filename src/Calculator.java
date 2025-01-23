9daf1abf2918512b8508236f1ef7f89182e8a21d
Shibin Shaji <shibin.shaji@dcmail.ca>
2025-01-23 10:35:21 -0500

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        int c = a+b;
+        return c;
+    }
+
+    public int subtract(int a, int b) {
+        int c = a-b;
+        return c;
+    }
+    public int multiplication(int a, int b) {
+        int c = a * b;
+        return c;
+    }
+    public int division(int a, int b){
+        int c = a/b;
+        return c;
+    }
+}
